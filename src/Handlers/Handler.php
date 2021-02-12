ea0c75018ac1d2e853d7856732c3a47525b4be9b
SergiX44 <sergio@brighenti.me>
2021-02-12 22:27:39 +0100

    Added parameter matching

--- src/Handlers/Handler.php
+++ src/Handlers/Handler.php
@@ -10,9 +10,20 @@ use SergiX44\Nutgram\Nutgram;
 class Handler extends MiddlewareChain
 {
     /**
+     * regular expression to capture named parameters but not quantifiers
+     * captures {name}, but not {1}, {1,}, or {1,2}.
+     */
+    protected const PARAM_NAME_REGEX = '/\{((?:(?!\d+,?\d+?)\w)+?)\}/';
+
+    /**
      * @var string|null
      */
     private ?string $pattern;
+
+    /**
+     * @var array
+     */
+    private array $parameters;
 
     private $callable;
 
@@ -29,6 +40,24 @@ class Handler extends MiddlewareChain
     }
 
     /**
+     * @param  string  $pattern
+     * @param  string  $value
+     * @return bool
+     */
+    public function matching(string $pattern, string $value): bool
+    {
+        $pattern = str_replace('/', '\/', $pattern);
+        $regex = '/^'.preg_replace(self::PARAM_NAME_REGEX, '(?<$1>.*)', $pattern).' ?$/miu';
+
+        $regexMatched = (bool) preg_match($regex, $value, $matches);
+        if ($regexMatched) {
+            $this->parameters = array_slice(array_unique($matches), 1);
+        }
+
+        return $regexMatched;
+    }
+
+    /**
      * @param  Nutgram  $bot
      * @return mixed
      * @throws \DI\DependencyException
@@ -36,6 +65,6 @@ class Handler extends MiddlewareChain
      */
     public function __invoke(Nutgram $bot)
     {
-        return call_user_func($bot->resolve($this->callable), $bot);
+        return call_user_func($bot->resolve($this->callable), $bot, ...$this->parameters);
     }
 }
