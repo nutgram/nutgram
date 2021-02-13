e78e5fe0d946e761ef94bfed0a28f500fc4dec0d
Sergio Brighenti <sergio@brighenti.me>
2021-02-13 20:38:20 +0100

    Running mode webhook

--- src/RunningMode/Webhook.php
+++ src/RunningMode/Webhook.php
@@ -3,12 +3,36 @@
 
 namespace SergiX44\Nutgram\RunningMode;
 
+use JsonMapper;
 use SergiX44\Nutgram\Nutgram;
+use SergiX44\Nutgram\Telegram\Types\Update;
 
 class Webhook implements RunningMode
 {
+
+    /**
+     * @var JsonMapper
+     */
+    private JsonMapper $mapper;
+
+    /**
+     * Webhook constructor.
+     * @param  JsonMapper  $mapper
+     */
+    public function __construct(JsonMapper $mapper)
+    {
+        $this->mapper = $mapper;
+    }
+
+    /**
+     * @param  Nutgram  $bot
+     * @throws \JsonMapper_Exception
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     */
     public function processUpdates(Nutgram $bot)
     {
-        // TODO: Implement run() method.
+        $input = file_get_contents('php://input');
+        $update = $this->mapper->map(json_decode($input), new Update());
+        $bot->processUpdate($update);
     }
 }
