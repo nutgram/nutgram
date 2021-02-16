f0c46411fae3d2e43f63b2834694904d12173db5
Sergio Brighenti <sergio@brighenti.me>
2021-02-16 21:04:44 +0100

    Completed shortcuts methods

--- src/Proxies/UpdateProxy.php
+++ src/Proxies/UpdateProxy.php
@@ -5,8 +5,13 @@ namespace SergiX44\Nutgram\Proxies;
 
 use SergiX44\Nutgram\Telegram\Types\CallbackQuery;
 use SergiX44\Nutgram\Telegram\Types\Chat;
+use SergiX44\Nutgram\Telegram\Types\ChosenInlineResult;
 use SergiX44\Nutgram\Telegram\Types\InlineQuery;
 use SergiX44\Nutgram\Telegram\Types\Message;
+use SergiX44\Nutgram\Telegram\Types\Poll;
+use SergiX44\Nutgram\Telegram\Types\PollAnswer;
+use SergiX44\Nutgram\Telegram\Types\PreCheckoutQuery;
+use SergiX44\Nutgram\Telegram\Types\ShippingQuery;
 use SergiX44\Nutgram\Telegram\Types\Update;
 use SergiX44\Nutgram\Telegram\Types\User;
 
@@ -79,5 +84,45 @@ trait UpdateProxy
     public function inlineQuery(): ?InlineQuery
     {
         return $this->update?->inline_query;
+    }
+
+    /**
+     * @return ChosenInlineResult|null
+     */
+    public function chosenInlineResult(): ?ChosenInlineResult
+    {
+        return $this->update?->chosen_inline_result;
+    }
+
+    /**
+     * @return ShippingQuery|null
+     */
+    public function shippingQuery(): ?ShippingQuery
+    {
+        return $this->update?->shipping_query;
+    }
+
+    /**
+     * @return PreCheckoutQuery|null
+     */
+    public function preCheckoutQuery(): ?PreCheckoutQuery
+    {
+        return $this->update?->pre_checkout_query;
+    }
+
+    /**
+     * @return Poll|null
+     */
+    public function poll(): ?Poll
+    {
+        return $this->update?->poll;
+    }
+
+    /**
+     * @return PollAnswer|null
+     */
+    public function pollAnswer(): ?PollAnswer
+    {
+        return $this->update?->poll_answer;
     }
 }
