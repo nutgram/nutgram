02cdb6146aa31ac5986e4f3919531d2c48894e07
Luca Patera <lucapatera@outlook.it>
2021-10-02 20:44:54 +0200

    Grouped types: Command

--- src/Telegram/Types/Command/BotCommandScopeChatMember.php
+++ src/Telegram/Types/Command/BotCommandScopeChatMember.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace SergiX44\Nutgram\Telegram\Types;
+namespace SergiX44\Nutgram\Telegram\Types\Command;
 
 /**
  * Represents the {@see https://core.telegram.org/bots/api#botcommandscope scope}
