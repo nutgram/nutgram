176b834201cae17be642f1d1cb9ad0f8761e52dc
Sergio Brighenti <sergio@brighenti.me>
2022-03-13 16:48:17 +0100

    simplify call to the bot instance

--- src/Telegram/Types/BaseType.php
+++ src/Telegram/Types/BaseType.php
@@ -5,14 +5,36 @@ namespace SergiX44\Nutgram\Telegram\Types;
 use Illuminate\Support\Traits\Macroable;
 use SergiX44\Nutgram\Nutgram;
 
+/**
+ * @mixin Nutgram
+ */
 abstract class BaseType
 {
-    use Macroable;
+    use Macroable {
+        __call as macroCall;
+    }
 
     protected ?Nutgram $bot;
 
+    /**
+     * @param  Nutgram|null  $bot
+     */
     public function __construct(?Nutgram $bot = null)
     {
         $this->bot = $bot;
+    }
+
+    /**
+     * @param  string  $method
+     * @param  array  $parameters
+     * @return mixed
+     */
+    public function __call(string $method, array $parameters)
+    {
+        if (method_exists($this->bot, $method)) {
+            return $this->bot->$method(...$parameters);
+        }
+
+        return $this->macroCall($method, $parameters);
     }
 }
