d75458e24865ea77dca015c9ea92a2e3dcf2ddfe
SergiX44 <sergio@brighenti.me>
2022-05-13 11:23:25 +0200

    serialization issues & constructor

--- src/Telegram/Types/WebApp/WebAppInfo.php
+++ src/Telegram/Types/WebApp/WebAppInfo.php
@@ -14,5 +14,14 @@ class WebAppInfo extends BaseType
      * An HTTPS URL of a Web App to be opened with additional data
      * as specified in {@see https://core.telegram.org/bots/webapps#initializing-web-apps Initializing Web Apps}
      */
-    public string $url;
+    public ?string $url;
+
+    /**
+     * @param  string|null  $url
+     */
+    public function __construct(?string $url = null)
+    {
+        parent::__construct();
+        $this->url = $url;
+    }
 }
