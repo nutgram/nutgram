21d48260bb3718c9d29c6540b07a38099332715f
Sergio Brighenti <SergiX44@users.noreply.github.com>
2022-07-21 13:14:57 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Exception/CannotSerializeException.php
+++ src/Exception/CannotSerializeException.php
@@ -7,7 +7,6 @@ use Throwable;
 
 class CannotSerializeException extends Exception
 {
-
     public function __construct(string $message = 'Cannot serialize. Please remove closures and try again.', int $code = 0, ?Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
