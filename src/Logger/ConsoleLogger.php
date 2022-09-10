08b8c6b8690e8856cb1fda7020741725eb361c1e
Luca Patera <lucapatera@outlook.it>
2022-09-10 19:24:08 +0200

    F for Fix

--- src/Logger/ConsoleLogger.php
+++ src/Logger/ConsoleLogger.php
@@ -13,23 +13,17 @@ class ConsoleLogger extends AbstractLogger
             return;
         }
 
+        $stringContext = trim(json_encode($context));
+
+        if (!empty($stringContext)) {
+            $message .= "\n{$stringContext}";
+        }
+
         print(sprintf(
             "[%s] %s: %s\n\n",
             date('Y-m-d H:i:s'),
             strtoupper($level),
-            $this->interpolate($message, $context)
+            $message,
         ));
-    }
-
-    private function interpolate(Stringable|string $message, array $context): string
-    {
-        $replace = [];
-        foreach ($context as $key => $val) {
-            if (!is_array($val) && (!is_object($val) || method_exists($val, '__toString'))) {
-                $replace['{'.$key.'}'] = $val;
-            }
-        }
-
-        return strtr($message, $replace);
     }
 }
