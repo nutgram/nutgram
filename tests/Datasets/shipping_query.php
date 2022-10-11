e882511593f1cf78ed95404360042f4bb69d916d
Luca Patera <Lukasss93@users.noreply.github.com>
2022-10-11 21:21:44 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/Datasets/shipping_query.php
+++ tests/Datasets/shipping_query.php
@@ -1,6 +1,6 @@
 <?php
 
-dataset('shipping_query', function(){
+dataset('shipping_query', function () {
     $file = file_get_contents(__DIR__.'/../Updates/shipping_query.json');
 
     return [json_decode($file)];
