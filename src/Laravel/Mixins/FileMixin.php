042444fcaa57c5a78cc14ad568e4459242bf5025
Luca Patera <lucapatera@outlook.it>
2022-10-15 20:22:51 +0200

    Remove duplicated code

--- src/Laravel/Mixins/FileMixin.php
+++ src/Laravel/Mixins/FileMixin.php
@@ -2,10 +2,7 @@
 
 namespace SergiX44\Nutgram\Laravel\Mixins;
 
-use Illuminate\Http\File as LaravelFile;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Str;
-use Psr\Http\Client\ClientInterface;
+use Closure;
 use SergiX44\Nutgram\Nutgram;
 use SergiX44\Nutgram\Telegram\Types\Media\File;
 
@@ -15,32 +12,14 @@ use SergiX44\Nutgram\Telegram\Types\Media\File;
 class FileMixin
 {
     /**
-     * @return \Closure
+     * @return Closure
      */
-    public function saveToDisk()
+    public function saveToDisk(): Closure
     {
-        return function (string $path, string $disk = null, array $clientOpt = []): bool {
+        return function (string $path, ?string $disk = null, array $clientOpt = []): bool {
             /** @var File $this */
 
-            $storage = Storage::disk($disk);
-
-            if (Str::endsWith($path, ['/', '\\'])) {
-                $path .= basename($this->file_path ?? $this->file_id);
-            }
-
-            if ($this->getConfig()['is_local'] ?? false) {
-                return $storage->put($path, $this->downloadUrl($this));
-            }
-
-            //create temp file
-            $tmpFile = tempnam(sys_get_temp_dir(), uniqid(strftime('%G-%m-%d')));
-
-            //download file to temp file
-            $http = $this->getContainer()->get(ClientInterface::class);
-            $http->get($this->downloadUrl($this), array_merge(['sink' => $tmpFile], $clientOpt));
-
-            //save temp file to disk
-            return $storage->putFileAs('/', new LaravelFile($tmpFile), $path);
+            return MixinUtils::saveFileToDisk($this, $path, $disk, $clientOpt);
         };
     }
 }
--- src/Laravel/Mixins/NutgramMixin.php
+++ src/Laravel/Mixins/NutgramMixin.php
@@ -2,42 +2,21 @@
 
 namespace SergiX44\Nutgram\Laravel\Mixins;
 
-use Illuminate\Http\File as LaravelFile;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Str;
-use Psr\Http\Client\ClientInterface;
+use Closure;
 use SergiX44\Nutgram\Nutgram;
 use SergiX44\Nutgram\Telegram\Types\Media\File;
 
 class NutgramMixin
 {
     /**
-     * @return \Closure
+     * @return Closure
      */
-    public function downloadFileToDisk()
+    public function downloadFileToDisk(): Closure
     {
-        return function (File $file, string $path, string $disk = null, array $clientOpt = []): bool {
+        return function (File $file, string $path, ?string $disk = null, array $clientOpt = []): bool {
             /** @var Nutgram $this */
 
-            $storage = Storage::disk($disk);
-
-            if (Str::endsWith($path, ['/', '\\'])) {
-                $path .= basename($file->file_path ?? $file->file_id);
-            }
-
-            if ($this->getConfig()['is_local'] ?? false) {
-                return $storage->put($path, $this->downloadUrl($file));
-            }
-
-            //create temp file
-            $tmpFile = tempnam(sys_get_temp_dir(), uniqid(strftime('%G-%m-%d')));
-
-            //download file to temp file
-            $http = $this->getContainer()->get(ClientInterface::class);
-            $http->get($this->downloadUrl($file), array_merge(['sink' => $tmpFile], $clientOpt));
-
-            //save temp file to disk
-            return $storage->putFileAs('/', new LaravelFile($tmpFile), $path);
+            return MixinUtils::saveFileToDisk($file, $path, $disk, $clientOpt);
         };
     }
 }
