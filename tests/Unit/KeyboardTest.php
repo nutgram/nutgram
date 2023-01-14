4c85339c380ec983f75978e3adc92b93defeefaf
Luca Patera <lucapatera@outlook.it>
2023-01-14 12:21:55 +0100

    Fix missing is_persistent parameter in class constructor/make

--- tests/Unit/KeyboardTest.php
+++ tests/Unit/KeyboardTest.php
@@ -22,3 +22,40 @@ it('make a correct keyboard', function () {
 
     expect(json_encode($keyboard))->toBe(json_encode($expected));
 });
+
+it('makes a correct ReplyKeyboardMarkup', function () {
+    $expected = [
+        'keyboard' => [
+            [
+                ['text' => 'test'],
+            ],
+            [
+                [
+                    'text' => 'send contact',
+                    'request_contact' => true,
+                ],
+            ],
+        ],
+        'is_persistent' => true,
+        'resize_keyboard' => true,
+        'one_time_keyboard' => true,
+        'input_field_placeholder' => 'test',
+        'selective' => true,
+    ];
+
+    $keyboard = ReplyKeyboardMarkup::make(
+        resize_keyboard: true,
+        one_time_keyboard: true,
+        input_field_placeholder: 'test',
+        selective: true,
+        is_persistent: true,
+    )
+        ->addRow(
+            KeyboardButton::make('test'),
+        )
+        ->addRow(
+            KeyboardButton::make('send contact', request_contact: true),
+        );
+
+    expect(json_encode($keyboard))->toBe(json_encode($expected));
+});
