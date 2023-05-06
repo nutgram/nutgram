ea39a219326d6be9b4d088f468a9e6d10c4c21f4
Luca Patera <lucapatera@outlook.it>
2023-05-07 00:06:43 +0200

    Remove properties

--- src/Telegram/Types/Input/InputMedia.php
+++ src/Telegram/Types/Input/InputMedia.php
@@ -20,18 +20,4 @@ abstract class InputMedia extends BaseType
      * Type of the result
      */
     public InputMediaType $type;
-
-    /**
-     * File to send. Pass a file_id to send a file that exists on the Telegram servers (recommended),
-     * pass an HTTP URL for Telegram to get a file from the Internet, or pass “attach://<file_attach_name>”
-     * to upload a new one using multipart/form-data under <file_attach_name> name.
-     * @see https://core.telegram.org/bots/api#sending-files More info on Sending Files
-     * @var string|resource $media
-     */
-    public mixed $media;
-
-    /**
-     * Optional. Caption of the photo to be sent, 0-1024 characters after entities parsing
-     */
-    public ?string $caption = null;
 }
