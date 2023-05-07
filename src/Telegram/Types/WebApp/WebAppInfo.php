febf263adee52c0cd320c24fddc460aa7e99eddf
Sergio Brighenti <sergio@brighenti.me>
2023-05-07 19:30:11 +0200

    test

--- src/Telegram/Types/WebApp/WebAppInfo.php
+++ src/Telegram/Types/WebApp/WebAppInfo.php
@@ -14,6 +14,11 @@ class WebAppInfo extends BaseType
     public string $url;
 
     public function __construct(string $url = '')
+
+
+
+
+
     {
         parent::__construct();
         $this->url = $url;
