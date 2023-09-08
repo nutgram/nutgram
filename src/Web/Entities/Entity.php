11f57e0cf24396c3243be25ba6fefda9b10fad75
Luca Patera <lucapatera@outlook.it>
2023-09-08 02:22:03 +0200

    Recursive toArray()

--- src/Web/Entities/Entity.php
+++ src/Web/Entities/Entity.php
@@ -22,7 +22,13 @@ abstract class Entity
 
     public function toArray(): array
     {
-        return get_object_vars($this);
+        $data = get_object_vars($this);
+        foreach ($data as $key => $value) {
+            if (is_subclass_of($value, __CLASS__)) {
+                $data[$key] = $value->toArray();
+            }
+        }
+        return $data;
     }
 
     protected function cast(): array
