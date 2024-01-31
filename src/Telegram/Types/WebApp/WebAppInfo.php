7e8ab41ff2ab041c67ba10436a12f2e91d26b242
Luca Patera <lucapatera@outlook.it>
2024-01-31 01:26:38 +0100

    Fix WebAppInfo exception

--- src/Telegram/Types/WebApp/WebAppInfo.php
+++ src/Telegram/Types/WebApp/WebAppInfo.php
@@ -2,12 +2,14 @@
 
 namespace SergiX44\Nutgram\Telegram\Types\WebApp;
 
+use SergiX44\Hydrator\Annotation\SkipConstructor;
 use SergiX44\Nutgram\Telegram\Types\BaseType;
 
 /**
  * Describes a {@see https://core.telegram.org/bots/webapps Web App}.
  * @see https://core.telegram.org/bots/api#webappinfo
  */
+#[SkipConstructor]
 class WebAppInfo extends BaseType
 {
     /** An HTTPS URL of a Web App to be opened with additional data as specified in {@see https://core.telegram.org/bots/webapps#initializing-web-apps Initializing Web Apps} */
