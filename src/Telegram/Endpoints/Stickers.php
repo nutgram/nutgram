30f12fa8f8a13a7be97f20468305b42810bf26c7
Luca Patera <lucapatera@outlook.it>
2024-04-03 00:11:10 +0200

    Add format field in setStickerSetThumbnail method

--- src/Telegram/Endpoints/Stickers.php
+++ src/Telegram/Endpoints/Stickers.php
@@ -292,6 +292,7 @@ trait Stickers
      * Returns True on success.
      * @see https://core.telegram.org/bots/api#setstickersetthumbnail
      * @param string $name Sticker set name
+     * @param StickerType|string $format Format of the thumbnail, must be one of “static” for a .WEBP or .PNG image, “animated” for a .TGS animation, or “video” for a WEBM video
      * @param int|null $user_id User identifier of the sticker set owner
      * @param InputFile|string|null $thumbnail {@see https://core.telegram.org/stickers#animated-sticker-requirements }{@see https://core.telegram.org/stickers#video-sticker-requirements }A .WEBP or .PNG image with the thumbnail, must be up to 128 kilobytes in size and have a width and height of exactly 100px, {@see https://core.telegram.org/stickers#animated-sticker-requirements https://core.telegram.org/stickers#animated-sticker-requirements}(see https://core.telegram.org/stickers#animated-sticker-requirements for animated sticker technical requirements), or a WEBM video with the thumbnail up to 32 kilobytes in size; see {@see https://core.telegram.org/stickers#video-sticker-requirements https://core.telegram.org/stickers#video-sticker-requirements} for video sticker technical requirements. Pass a file_id as a String to send a file that already exists on the Telegram servers, pass an HTTP URL as a String for Telegram to get a file from the Internet, or upload a new one using multipart/form-data. {@see https://core.telegram.org/bots/api#sending-files More information on Sending Files »}. Animated and video sticker set thumbnails can't be uploaded via HTTP URL. If omitted, then the thumbnail is dropped and the first sticker is used as the thumbnail.
      * @param array $clientOpt Client options
@@ -299,12 +300,13 @@ trait Stickers
      */
     public function setStickerSetThumbnail(
         string $name,
+        StickerType|string $format,
         ?int $user_id = null,
         InputFile|string|null $thumbnail = null,
         array $clientOpt = [],
     ): ?bool {
         $user_id ??= $this->userId();
-        $parameters = compact('name', 'user_id', 'thumbnail');
+        $parameters = compact('name', 'format', 'user_id', 'thumbnail');
 
         return $this->requestMultipart(__FUNCTION__, $parameters, options: $clientOpt);
     }
