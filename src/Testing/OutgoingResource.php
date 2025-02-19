a934d3d0ffb33a3a1591113ad000e0cf14908a6c
Luca Patera <lucapatera@outlook.it>
2025-02-19 20:52:45 +0100

    chore: oops

--- src/Testing/OutgoingResource.php
+++ src/Testing/OutgoingResource.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace SergiX44\Nutgram\Testing;
 
 use JsonSerializable;
-use Override;
 use Psr\Http\Message\StreamInterface;
 
 class OutgoingResource implements JsonSerializable
