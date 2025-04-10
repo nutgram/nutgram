f2ca20de71c2925bbc9d40ce907636f1c5751407
Luca Patera <lucapatera@outlook.it>
2025-04-11 00:27:17 +0200

    tests: add json serializable test to buildable types

--- tests/Feature/ArchTest.php
+++ tests/Feature/ArchTest.php
@@ -1,12 +1,15 @@
 <?php
 
 use SergiX44\Hydrator\Annotation\ConcreteResolver;
+use SergiX44\Nutgram\Nutgram;
 use SergiX44\Nutgram\Telegram\Types\BaseType;
+use SergiX44\Nutgram\Telegram\Types\Common\Response;
 use SergiX44\Nutgram\Telegram\Types\Inline\InlineQueryResult;
 use SergiX44\Nutgram\Telegram\Types\Input\InputMedia;
 use SergiX44\Nutgram\Telegram\Types\Input\InputMessageContent;
 use SergiX44\Nutgram\Telegram\Types\Input\InputPaidMedia;
 use SergiX44\Nutgram\Telegram\Types\Passport\PassportElementError;
+use SergiX44\Nutgram\Testing\TypeFaker;
 use SergiX44\Nutgram\Tests\TelegramTypeContext;
 
 arch('check that the code is using strict types')
@@ -61,5 +64,30 @@ test('check that Telegram Buildable Types does not have a "make" method', functi
         }
 
         expect($hasMakeMethod)->toBeFalse(sprintf('%s has a make method', $type->getName()));
+    }
+});
+
+test('check that Telegram types are json serializable (+ array_filter_null)', function () {
+    $typeFaker = Nutgram::fake()->getContainer()->get(TypeFaker::class);
+
+    $allowed = array_map(
+        fn (ReflectionClass $class) => $class->getName(),
+        getTelegramTypes(TelegramTypeContext::NonAbstract)
+    );
+
+    $ignore = [
+        Response::class,
+    ];
+
+    foreach ($allowed as $type) {
+        if (in_array($type, $ignore, true)) {
+            continue;
+        }
+
+        $result = $typeFaker->fakeInstanceOf($type);
+
+        expect($result)->toBeInstanceOf($type);
+
+        expect(json_encode($result))->toBe(json_encode($result->toArray()));
     }
 });
