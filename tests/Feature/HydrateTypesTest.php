21127d29f6ab7ea7db5806f070c6ae7e4b8290ad
Luca Patera <lucapatera@outlook.it>
2026-07-24 23:49:33 +0200

    test: rename and update dataset reference due to pest 3 bug

--- tests/Feature/HydrateTypesTest.php
+++ tests/Feature/HydrateTypesTest.php
@@ -10,4 +10,4 @@ it('hydrate rich_message', function ($content) {
     $result = $hydrator->hydrate($content, Message::class);
 
     expect($result)->toBeInstanceOf(Message::class);
-})->with('messages.rich_message');
+})->with('message.rich_message');
